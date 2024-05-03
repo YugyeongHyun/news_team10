d9596e727d7266d22ac828599b0424f31b0fd89d
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-03 19:19:58 +0900

    url 추가

--- article/serializers.py
+++ article/serializers.py
@@ -2,11 +2,22 @@ from rest_framework import serializers
 from .models import Article
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    pass
+# class CommentSerializer(serializers.ModelSerializer):
+#     replies = serializers.SerializerMethodField()
 
+#     class Meta:
+#         model = Comment
+#         fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'author', 'parent', 'replies']
+
+#     def get_replies(self, obj):
+#         # 대댓글은 'replies' 필드를 통해 직렬화
+#         if obj.replies.exists():
+#             return CommentSerializer(obj.replies.all(), many=True).data
+#         return []
 
 class ArticleSerializer(serializers.ModelSerializer):
+    # url = serializers.HyperlinkedIdentityField(view_name='article-detail')
+    # author =serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Article
         fields = ('title', 'content', 'created_at',
