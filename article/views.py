023ef16526a1989b940e9b81943f048d59f1e033
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-05-03 15:54:10 +0900

    migration complete

--- article/views.py
+++ article/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from article.models import Article
+from article.serializers import ArticleSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class ArticleListAPIView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)  # manu=True
+        return Response(serializer.data)
+
+    def post(self, request):
+        # 권한 미제시
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):  # raise_exception
+            serializer.save()
+            return Response(serializer.data, stauts=status.HTTP_201_CREATED)
+
+
+class ArticleDetailAPIView(APIView):
+    # 권한 미제시
+    pass
