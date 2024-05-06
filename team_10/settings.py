014967856a4ea8cee8bb8d07f36fa47f0e9128c6
alstj0606 <alstjwjddus0606@naver.com>
2024-05-07 06:42:46 +0900

    logout

--- team_10/settings.py
+++ team_10/settings.py
@@ -118,6 +118,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=200),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 
