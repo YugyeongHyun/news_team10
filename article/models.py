70d94dc8b4e08ccf3654882271ba12cbb847c73b
LaisKor <spiderjiwon@nate.com>
2024-05-08 13:51:55 +0900

    article models.py 중복 수정

--- article/models.py
+++ article/models.py
@@ -1,24 +1,13 @@
 from django.conf import settings
 from django.db import models
-from account.models import User
-
-
-
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 작성자 필드 추가
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
     url = models.URLField(max_length=200, blank=True, null=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_articles")
-
+    
     def __str__(self):
         return self.title
-
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    url = models.URLField(max_length=200, blank=True, null=True)
-    like_users = models.ManyToManyField(
-        User, related_name="like_articles")
-
